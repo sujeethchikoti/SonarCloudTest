ac659038921f96f2a3890e8edddfaafa5a688163
sujeethc <sujeethc@10.4.33.59>
2023-10-12 11:09:52 +0530

    Sonar dummy test 3

--- src/main/java/com/infinite/sq/SonarTestClass.java
+++ src/main/java/com/infinite/sq/SonarTestClass.java
@@ -2,7 +2,7 @@ package com.infinite.sq;
 
 public class SonarTestClass {
 	public static void main(String[] args) {
-		String a = "Hello";
+		String a = "Hi";
 		System.out.println(a + "World");
 	}
 }
